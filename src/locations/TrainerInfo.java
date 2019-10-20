882fb46b12f6fe085137f214babf81e954f55da8
RahilSagarwala <r_sagarwala@mail.fhsu.edu>
2019-10-20 15:44:34 -0600

    Added Gym Leader Names
    *Added Japanese Text for Trainer Info

--- src/locations/TrainerInfo.java
+++ src/locations/TrainerInfo.java
@@ -66,16 +66,76 @@ public class TrainerInfo extends JPanel {
 		 badgeLabel.setForeground(Color.LIGHT_GRAY);
 		 badgeLabel.setFont(font);
 		 
+		 JLabel badge1Label = new JLabel("1. Brock    ");
+		 badge1Label.setForeground(Color.LIGHT_GRAY);
+		 badge1Label.setFont(font);
+		 
+		 JLabel badge2Label = new JLabel("2. Misty    ");
+		 badge2Label.setForeground(Color.LIGHT_GRAY);
+		 badge2Label.setFont(font);
+		 
+		 JLabel badge3Label = new JLabel("3. Lt. Surge    ");
+		 badge3Label.setForeground(Color.LIGHT_GRAY);
+		 badge3Label.setFont(font);
+		 
+		 JLabel badge4Label = new JLabel("4. Erika    ");
+		 badge4Label.setForeground(Color.LIGHT_GRAY);
+		 badge4Label.setFont(font);
+		 
+		 JLabel badge5Label = new JLabel("5. Koga    ");
+		 badge5Label.setForeground(Color.LIGHT_GRAY);
+		 badge5Label.setFont(font);
+		 
+		 JLabel badge6Label = new JLabel("6. Sabrina    ");
+		 badge6Label.setForeground(Color.LIGHT_GRAY);
+		 badge6Label.setFont(font);
+		 
+		 JLabel badge7Label = new JLabel("7. Blaine    ");
+		 badge7Label.setForeground(Color.LIGHT_GRAY);
+		 badge7Label.setFont(font);
+		 
+		 JLabel badge8Label = new JLabel("8. ?    ");
+		 badge8Label.setForeground(Color.LIGHT_GRAY);
+		 badge8Label.setFont(font);
+		 
+		 JPanel badgePanel = new JPanel();
+		 badgePanel.setBackground(Color.black);
+		 badgePanel.setLayout(new GridLayout(2,4));
+		 badgePanel.add(badge1Label);
+		 badgePanel.add(badge2Label);
+		 badgePanel.add(badge3Label);
+		 badgePanel.add(badge4Label);
+		 badgePanel.add(badge5Label);
+		 badgePanel.add(badge6Label);
+		 badgePanel.add(badge7Label);
+		 badgePanel.add(badge8Label);
+		 
+		 
+		 
 	     	     
 	     
 	     if (language == "Japanese") {
 	    	 returnButton.setText("とじる");
-
+	    	 nameLabel.setText("なまえ：　" + player.getName());
+//	    	 idLabel.setText("");
+	    	 badgeLabel.setText("ポケモン　バッジ：　" + player.getBadeNumber());
+	    	 moneyLabel.setText("おこづかい：　" + player.getMoney() + "￥");
+	    	 badge1Label.setText("1. タケシ    ");
+	    	 badge2Label.setText("2. カスミ    ");
+	    	 badge3Label.setText("3. マチス    ");
+	    	 badge4Label.setText("4. エリカ    ");
+	    	 badge5Label.setText("5. キョウ     ");
+	    	 badge6Label.setText("6. ナツメ    ");
+	    	 badge7Label.setText("7. カツラ    ");
+	    	 badge8Label.setText("8. ?    ");
+	    	 
 	     }
+	     
+	    
 	     
 	     gb.gridx=0;
 	     gb.gridy=0;
-	     gb.insets = new Insets(-200,0,0,0);
+	     gb.insets = new Insets(-125,0,0,0);
 		 add(returnButton, gb);
 		 
 		 gb.gridx=0;
@@ -94,6 +154,10 @@ public class TrainerInfo extends JPanel {
 		 gb.gridx=0;
 	     gb.gridy=4;
 		 add(badgeLabel, gb);
+		 
+		 gb.gridx=0;
+	     gb.gridy=5;
+		 add(badgePanel, gb);
 		 
 
 		 

b5346b677e058fb8a2143647691fc0a21199ca1e
RahilSagarwala <r_sagarwala@mail.fhsu.edu>
2019-11-10 16:55:22 -0700

    More bug fixes for default sound off

--- src/mainGame/Options.java
+++ src/mainGame/Options.java
@@ -363,7 +363,7 @@ public class Options extends JPanel {
 	            		 try {
 							Clip clip = null;
 							mg = new MainGame(cl, cards, font1, language1, textSpeed1, stopTimer, titleSize, buttonWidth, 
-							    		buttonHeight, player, rival, professorOakVisited, labOutsideButtonEnable, color, clip, "on", "no");
+							    		buttonHeight, player, rival, professorOakVisited, labOutsideButtonEnable, color, clip, "off", "no");
 						} catch (LineUnavailableException e1) {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
